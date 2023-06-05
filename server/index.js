daa41c4f44371f397ef9e6409768f8bff693584a
lokesh wani <51832692+stellar-droid@users.noreply.github.com>
2023-06-05 19:13:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,28 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const pool = require('./db1');
 
     //////middleware
 app.use(cors()); // allows us to parse json
 app.use(express.json()); // allows us to parse json
+
+////ROUTES
+  
+//create a todo
+app.post("/todos", async(req, res) => {
+
+// get all todos
+
+// update a todo
+
+// delete a todo
 
 
 
 app.listen(3000, () => {
    console.log('Server is running on port 3000');
 });
+});app.listen(3001, () => {
+    console.log('Server is running on port 3001'  );
+ });
